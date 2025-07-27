3a6404183f6979f2031f76a129a6f4ff167c21f5
Zndrr <184755155+zndrCode@users.noreply.github.com>
2025-07-27 19:56:51 +0800

    Added displayweather

--- script.js
+++ script.js
@@ -30,3 +30,61 @@ function getWeather(){
         alert('Error fetching hourly forecast data. Please try again.');
     });
 }
+
+function displayWeather(dta) {
+
+    const tempDivInfo = document.getElementById('temp-div');
+    const weatherInfoDiv = document.getElementById('weather-info');
+    const weatherIcon = document.getElementById('weather-icon');
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
+
+    if (data.cod === '404') {
+        weatherInfoDiv.innerHTML = '<p>${data.message}</p>';
+    } else {
+        const cityName = data.name;
+        const temperature = Math.round(data.main.tmp - 273.15);
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = 'https://openweathermap.org/img/wn/${iconCode}@4x.png';
+
+        const temperatureHTML = '
+        <p>${temperature}°C</p>
+        ';
+
+        const weatherHTML = '
+            <p>${cityName}</p>
+            <p>${description}</p>
+        ';
+
+        tempDivInfo.innerHTML = temperatureHTML;
+        weatherInfoDiv.innerHTML = weatherHTML;
+        weatherIcon.src = iconUrl;
+        weatherIcon.all = description;
+
+        showImage();
+    }
+}
+
+function displayHourlyForecast(hourlyData){
+
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
+    const next24hours = hourlyData.slice(0, 8);
+
+    next24hours.forEach(item => {
+
+        const dateTime = new Date(item.dt * 1000);
+        const hour = dateTime.getHours();
+        const temperature = Math.round(item.main.temp - 273.15);
+        const iconUrl = 'https://openweathermap.org/img/wn/${iconCode}.png';
+
+        const hourlyItemHtml = '
+            <div class="hourly-item">
+                <span>${hour}:00</span>
+                <img src="${iconUrl}" alt="Hourly Weather Icon">
+                <span>${temperature}°C</span>
+            </div>
+        ';
+        hourlyForecastDiv.innerHTML += hourlyItemHtml;
+
+    });
+}
